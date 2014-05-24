f841255a921d804f6c8a15c4041b9d03f67f6c9e
Adam Grandt <adam@inuwashi.net>
2014-05-24 13:00:33 -0400

    Added skype (and i386 packages) to install_staff

--- .install_stuff.sh
+++ .install_stuff.sh
@@ -1,10 +1,17 @@
+#Adding parteners for skype
+sudo dpkg --add-architecture i386
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
+
 sudo apt-get update
 sudo apt-get upgrade
 
 #Set up CLI
 sudo apt-get install emacs23-nox emacs23-el zsh screen most git 
+chsh -s /bin/zsh
+
 
 # Set up GUI
-sudo apt-get install chromium-browser ratpoison pepperflashplugin-nonfree flashplugin-nonfree 
-cp .ratpoison.desktop  /usr/share/xsessions/ratpoison.desktop
+sudo apt-get install chromium-browser ratpoison pepperflashplugin-nonfree flashplugin-installer skype libpulse0:i386
+sudo cp .ratpoison.desktop  /usr/share/xsessions/ratpoison.desktop
 
