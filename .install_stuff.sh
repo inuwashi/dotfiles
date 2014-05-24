e52e174372a11016b6f5d2b434575371169afe17
Adam Grandt <adam@inuwashi.net>
2014-05-24 12:47:24 -0400

    Seperated CLI and GUI in install_staff

--- .install_stuff.sh
+++ .install_stuff.sh
@@ -1,5 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get indtall emacs23-nox emacs23-el zsh screen most git chromium-browser ratpoison pepperflashplugin-nonfree flashplugin-nonfree 
+
+#Set up CLI
+sudo apt-get install emacs23-nox emacs23-el zsh screen most git 
+
+# Set up GUI
+sudo apt-get install chromium-browser ratpoison pepperflashplugin-nonfree flashplugin-nonfree 
 cp .ratpoison.desktop  /usr/share/xsessions/ratpoison.desktop
 
