341f0c12956c8721775be7748b0896435960308a
Adam Grandt <adam@inuwashi.net>
2014-05-24 02:35:27 -0400

    script to set up my env, lots of work left

---
+++ .install_stuff.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get indtall emacs23-nox emacs23-el zsh screen most git chromium-browser ratpoison pepperflashplugin-nonfree flashplugin-nonfree 
+cp .ratpoison.desktop  /usr/share/xsessions/ratpoison.desktop
+
